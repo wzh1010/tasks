373332a7bc03bf543a6df6c895b204cdd34f61fd
wzh1010 <31241389+wzh1010@users.noreply.github.com>
2017-12-01 19:11:43 +0800

    f

---
+++ js/桌游精灵.js
@@ -0,0 +1,14 @@
+var
+//var denote=document.getElementsByClassName("denote");
+var simple=document.getElementsByClassName("simple");
+
+
+simple.onclick=function(){
+	window.location.href="九宫格  css.html";
+}
+
+
+
+
+
+
