4a6ca2de727b5ffbbaaf71326a694714234fda35
wzh1010 <31241389+wzh1010@users.noreply.github.com>
2018-04-21 11:17:50 +0800

    1

--- js/radish/script/js-5 entry.js
+++ js/radish/script/js-5 entry.js
@@ -1,38 +1,22 @@
-var n = document.getElementById("name");
-var p = document.getElementById("name-pwd");
-var error = document.getElementById("error");
-console.log(n.value);
-
-function user() {
-    if (n.value==="admin" ||n.length===0 || n.value==="") {
-        $("#error").empty();
-        console.log(error);
-    }else {
-        error.innerHTML = "无此用户";
-        console.log(error);
-    }
-}
-function login() {
-    console.log(n.value);
-    console.log(p.value);
-    var xmlhttp = new XMLHttpRequest();
-
-    xmlhttp.open("Post","/carrots-admin-ajax/a/login",true);
-    xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-    xmlhttp.send("name=" + n.value + "&pwd=" + p.value);
-
-    xmlhttp.onreadystatechange = function()
-    {
-        if (xmlhttp.readyState === 4) {
-            if (xmlhttp.status === 200) {
-                var jsons = JSON.parse(xmlhttp.responseText);
-                console.log(jsons);
-                if (jsons.code === 0) {
-                    window.location.href = "https://www.jnshu.com/"
-                } else {
-                    alert("账号或密码错误")
-                }
-            }
-        }
-    };
-}
+$(document).ready(function(){
+    $("button").click(function(){
+        var n=$('#name').val();
+        var p=$('#name-pwd').val();
+        console.log(n);
+        console.log(p);
+        $.post("/carrots-admin-ajax/a/login", {
+            name: n,
+            pwd: p
+        },
+            function (data,status){
+               console.log(status);
+               var jsons=JSON.parse(data);
+               console.log(jsons.message);
+               if (jsons.code===0){
+                   window.location.href = "http://www.jnshu.com/home"
+               }else {
+                   $('#error').html(jsons.message);
+               }
+        });
+    });
+});
